8de751b7b8c1600f2534bfea381d9e27293c050f
Francisco Noel <byfnoel@gmail.com>
2022-07-02 20:00:53 -0700

    facts about react

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+export default function Main() {
+  return (
+    <main>
+      <h1>Few Facts About React</h1>
+      <ul>
+        <li>Initially released in 2013</li>
+        <li>Maintained by Facebook</li>
+        <li> 100k+ starts on GitHub</li>
+        <li> Most Popular frontend library in the world</li>
+      </ul>
+    </main>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <nav>
+      <img src="/favicon.ico" alt="logo" />
+      <h1>React Facts</h1>
+    </nav>
+  );
+}
